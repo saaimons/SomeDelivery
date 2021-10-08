ac67cc4ede9ec0b617abf6d006597ac327ac05cf
Rockstonebb <saaimonsg2@gmail.com>
2021-10-08 20:22:54 -0300

    Commit

---
+++ SomeDelivery/src/main/java/ar/com/phoenos/SomeDelivery/SomeDeliveryApplication.java
@@ -0,0 +1,13 @@
+package ar.com.phoenos.SomeDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeDeliveryApplication.class, args);
+	}
+
+}
---
+++ SomeDelivery/src/test/java/ar/com/phoenos/SomeDelivery/SomeDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.phoenos.SomeDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
